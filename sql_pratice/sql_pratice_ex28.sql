87273dde78d81003c1efad465722f00740882670
Clodoaldo Lucio <clodoaldoulias@hotmail.com>
2024-10-07 17:09:24 -0300

    SQL pratice 10

---
+++ sql_pratice/sql_pratice_ex28.sql
@@ -0,0 +1,25 @@
+/*
+Each admission costs $50 for patients without insurance, and $10 for patients with insurance. 
+All patients with an even patient_id have insurance.
+
+Give each patient a 'Yes' if they have insurance, and a 'No' if they don't have insurance. 
+Add up the admission_total cost for each has_insurance group.
+*/
+
+
+SELECT
+
+    (CASE
+    	WHEN CAST(patient_id AS INTEGER)%2 = 0 THEN 'Yes'
+		ELSE 'No'      
+    END) AS yes_or_no,
+    
+	SUM(CASE
+    	WHEN CAST(patient_id AS INTEGER)%2 = 0 THEN 10
+		ELSE 50    
+    END) AS cost
+    
+FROM 
+	admissions
+
+GROUP BY 1
