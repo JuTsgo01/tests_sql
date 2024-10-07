1e73998e48664704427a4c818f5c39e2799791aa
Clodoaldo Lucio <158162298+JuTsgo01@users.noreply.github.com>
2024-10-07 20:39:19 -0300

    Update sql_pratice_ex31.sql
    edit format

--- sql_pratice/sql_pratice_ex31.sql
+++ sql_pratice/sql_pratice_ex31.sql
@@ -4,8 +4,8 @@ Show the percent of patients that have 'M' as their gender. Round the answer to 
 
 SELECT
 	CONCAT(
-    ROUND(CAST(COUNT(CASE WHEN gender = "M" THEN 1 END) AS FLOAT) / CAST(COUNT(*) AS FLOAT), 4) * 100, "%")
-    AS percent
+    	ROUND(CAST(COUNT(CASE WHEN gender = "M" THEN 1 END) AS FLOAT) / CAST(COUNT(*) AS FLOAT), 4) * 100, "%")
+    	AS percent
     
 FROM
 	patients
