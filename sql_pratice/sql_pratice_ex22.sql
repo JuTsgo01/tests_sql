1ecc4bfaf9a633ac050070356187ea9ada3941ce
Clodoaldo Lucio <158162298+JuTsgo01@users.noreply.github.com>
2024-10-07 20:44:29 -0300

    Update sql_pratice_ex22.sql
    edit format

--- sql_pratice/sql_pratice_ex22.sql
+++ sql_pratice/sql_pratice_ex22.sql
@@ -8,12 +8,12 @@ Convert KG to pounds by multiplying by 2.205.
 
 SELECT 
 	first_name || ' ' || last_name AS full_name,
-    ROUND((height / 30.48), 1) AS height_feet,
-    ROUND((weight * 2.205), 0) AS weight_pounds,
-    birth_date,
-    CASE WHEN gender = 'M' THEN 'MALE'
-         WHEN gender  = 'F' THEN 'FEMALE' END
-         AS gender_full
+    	ROUND((height / 30.48), 1) AS height_feet,
+    	ROUND((weight * 2.205), 0) AS weight_pounds,
+    	birth_date,
+	CASE WHEN gender = 'M' THEN 'MALE'
+        WHEN gender  = 'F' THEN 'FEMALE' END
+        AS gender_full
     
     
 FROM 
