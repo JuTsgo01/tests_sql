ecb8d893ef704b77c047e2e09cebe65472239866
Clodoaldo Lucio <clodoaldoulias@hotmail.com>
2024-10-07 20:08:03 -0300

    SQL pratice 12

---
+++ sql_pratice/sql_pratice_ex31.sql
@@ -0,0 +1,11 @@
+/*
+Show the percent of patients that have 'M' as their gender. Round the answer to the nearest hundreth number and in percent form.
+*/
+
+SELECT
+	CONCAT(
+    ROUND(CAST(COUNT(CASE WHEN gender = "M" THEN 1 END) AS FLOAT) / CAST(COUNT(*) AS FLOAT), 4) * 100, "%")
+    AS percent
+    
+FROM
+	patients
